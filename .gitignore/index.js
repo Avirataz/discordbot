e334ef125c9e33c450880d434d278a5753dc0066
Avirataz <31666484+Avirataz@users.noreply.github.com>
2020-05-02 21:43:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,47 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true},{ fetchAllMembers: true, sync: true });
+const path = require('path');
+const sqlite = require('sqlite');
+const jimp = require('jimp');
+bot.commands = new Discord.Collection();
+let purple = botconfig.purple;
 
-client.on('ready', () => {
 
-    console.log('I am ready!');
+const moment = require('moment');
 
+
+var prefix = (botconfig.prefix)
+
+bot.on("ready", () => {
+  console.log(`SkrrrBot#5 a démarré, avec ${bot.users.size} utilisateurs, dans ${bot.channels.size} channels et ${bot.guilds.size} serveurs.`);
+  bot.user.setActivity("One Piece RP | Tatsuka", {type: "PLAYING"});
 });
 
-client.on('message', message => {
 
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => {
+	db.migrate().then(db => bot.database = db);
 });
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+fs.readdir("./commands/", (err, files) =>  {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.lenght <= 0){
+    console.log("Commande introuvable.")
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+		if (props.help && props.help.name) {
+			bot.commands.set(props.help.name, props);
+		}
+  });
+});
+
+
+bot.login(process.env.BOT_TOKEN);
