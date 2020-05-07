e31e29d8e3c89adacd0228292033afd091f16337
Avirataz <31666484+Avirataz@users.noreply.github.com>
2020-05-07 15:58:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,16 +1,32 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true},{ fetchAllMembers: true, sync: true });
+const bot = new Discord.Client({disableEveryone: true});
 const path = require('path');
 const sqlite = require('sqlite');
 const jimp = require('jimp');
 bot.commands = new Discord.Collection();
 let purple = botconfig.purple;
+let cooldown = new Set();
+let cdseconds = 5;
+const chratis_cooldown_time = 5;
 const money = require('discord-money');
 const moment = require('moment');
+const emoji = bot.emojis.get("559427989713190922");
+var spawning = "no";
+var cheerio = require("cheerio");
 var request = require("request");
+const yt = require("ytdl-core");
+const ffmpeg = require("ffmpeg");
+let queue = {};
 var prefix = (botconfig.prefix)
+var skipping = 0;
+var skips = 3;
+const m = require("moment-duration-format");
+let os = require('os')
+let cpuStat = require("cpu-stat")
+const ms = require("ms")
+var opusscript = require("opusscript");
 const {
     version
 } = require("discord.js");
@@ -42,6 +58,66 @@ fs.readdir("./commands/", (err, files) =>  {
 		}
   });
 });
+
+
+bot.on("message", async message => {
+  if (commands.hasOwnProperty(message.content.toLowerCase().slice(botconfig.prefix.length).split(' ')[0])) commands[message.content.toLowerCase().slice(botconfig.prefix.length).split(' ')[0]](message);
+  var parts = message.content.split(" ");
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+	let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+	if(!prefixes[message.guild.id]){
+		prefixes[message.guild.id] = {
+			prefixes: botconfig.prefix
+		};
+	}
+  let prefix = prefixes[message.guild.id].prefixes;
+  if(!message.content.startsWith(prefix)) return;
+  //if(cooldown.has(message.author.id)){
+    //message.delete();
+    //let cdembed = new Discord.RichEmbed()
+    //.setAuthor(message.author.username)
+    //.setColor(botconfig.red)
+    //.addField("❌Error", "You need to wait 5 secs between commands.");
+    //return message.channel.send(cdembed).then(message => {message.delete(3000)});1
+  //}
+  if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+  }
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+  setTimeout(() => {
+    cooldown.delete(message.author.id);
+}, chratis_cooldown_time * 1000);
+
+if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+
+let coinAmt = Math.floor(Math.random() * 15) + 1;
+let baseAmt = Math.floor(Math.random() * 15) + 1;
+console.log(`${coinAmt} ; ${baseAmt}`);
+
+
+
+
+
+//let prefix = prefixes[message.guild.id].prefixes;
+
+//let commandfile = bot.commands.get(cmd.slice(prefix.length));
+//if(commandfile && cmd.startsWith(prefix)) commandfile.run(bot,message,args);
+
+if(message.author.bot) return;
+
+var command = message.content.toLowerCase().slice(prefix.length).split(' ')[0];
+
+if (command === 'image'){
+  image(message, parts);
+
+ }
 
 bot.on('message', msg => {
   if (msg.content === '.wiki Mera Mera no Mi') {
