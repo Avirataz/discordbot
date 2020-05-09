9abcd705411999309adcc690c4355465f292a080
Avirataz <31666484+Avirataz@users.noreply.github.com>
2020-05-09 18:25:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const sqlite = require('sqlite');
 const jimp = require('jimp');
 bot.commands = new Discord.Collection();
-client.commands = new Discord.Collection();
 let cooldown = new Set();
 let cdseconds = 5;
 const chratis_cooldown_time = 5;
